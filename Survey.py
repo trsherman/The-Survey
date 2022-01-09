a33ab53994f109f81f2437d58590b4ad104c235e
trsherman <76744106+trsherman@users.noreply.github.com>
2022-01-09 13:35:10 -0600

    Add files via upload

---
+++ Survey.py
@@ -0,0 +1,31 @@
+'''
+Created on Jan 9, 2022
+
+A general survey
+
+@author: Trinity.s
+'''
+
+print("What is your name?")
+name=input()
+print("Hello," +name )
+
+print("What is your favorite color?")
+color=input()
+print("I thought so... your name suits the color, " + color)
+
+print("What city did you grow up in?")
+city=input()
+print("I know where that is.")
+
+print("What is the name of your 'best friend'?")
+friend=input()
+print("Odd...")
+
+print("What color is your hair?")
+hair=input()
+print("I knew it... would you ever consider dying it " +color)
+
+print("Do you mind?")
+answer=input()
+print("Good, a visit from your old 'friend' from" +city, "is something you can expect soon")
